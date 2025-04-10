c388447b4905802d1d90693ea277ebc48f895014
James Dismore <jamesdismore@outlook.com>
2025-04-10 10:31:24 +0100

    test1

---
+++ lib/program.py
@@ -0,0 +1,5 @@
+#A function called make_snippet that takes a string as an argument 
+# and returns the first five words and then a '...' if there are more 
+# than that.
+
+def make_snippet
---
+++ tests/test_program.py
@@ -0,0 +1,5 @@
+from lib.program import *
+
+def test_make_snippet_exists():
+    make_snippet()
+
